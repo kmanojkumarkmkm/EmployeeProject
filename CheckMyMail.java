1e2b41b4c44c25d659133345ab215b5845671f42
kmanojkumarkmkm <kmanojkumarkmkm@gmail.com>
2023-06-17 17:49:09 +0530

    its may change one

--- CheckMyMail.java
+++ CheckMyMail.java
@@ -6,14 +6,15 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class CheckMyMail {
-
+// 
 	public static void main(String[] args) throws IOException {
 
 		try {
 		File f = new File("C:\\Users\\Dell\\Documents\\myMail.txt");
 		FileReader fr = new FileReader(f);
 		int temp=0;
-		
+		// its my change 
+		int xyz=0;
 		while ((temp=fr.read())!=-1) {
 			System.out.print((char)temp);
 		}
